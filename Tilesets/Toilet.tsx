973ca6982c4adb96e27bcaf3cc1e7b12e767dab5
Justin Gunther <danwgun@gmail.com>
2019-11-07 09:26:19 -0600

    deleted unnecessary files

--- Tilesets/Toilet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="Toilet" tilewidth="24" tileheight="34" tilecount="1" columns="1">
- <image source="../LPC_city_inside/furniture/toilet.png" trans="003039" width="24" height="34"/>
-</tileset>
--- Tilesets/city_outside.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="city_outside" tilewidth="32" tileheight="32" tilecount="432" columns="24">
- <image source="../LPC_city_outside/city_outside.png" trans="050505" width="768" height="576"/>
-</tileset>
