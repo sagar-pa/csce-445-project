bf013793ddbae0e79944c166bf41c80adb4c47eb
Justin Gunther <danwgun@gmail.com>
2019-12-01 15:32:49 -0600

    added more fields and option selection

--- event/event.py
+++ event/event.py
@@ -4,6 +4,11 @@ import json
 
 import config
 
+class Option:
+    def __init__(self, text, trigger):
+        self.text = text
+        self.trigger = trigger
+
 class Event:
     font = pygame.font.Font(os.path.join('event', 'consolas.ttf'), 20)
     text_width, text_height = font.size(' ')
@@ -18,13 +23,16 @@ class Event:
             event_dict['text_only'],
             character_id=event_dict.get('character_id', None),
             hint=event_dict.get('hint', None),
-            entry=event_dict.get('entry', None),
-            trigger_event_id=event_dict.get('trigger_event_id', None),
+            trigger=event_dict.get('trigger', None),
             movements=event_dict.get('movements', None),
             options=event_dict.get('options', []),
             clue=event_dict.get('clue', None),
             teleport_to=event_dict.get('teleport_to', None),
-            trigger_on_done=event_dict.get('trigger_on_done', None)
+            trigger_on_done=event_dict.get('trigger_on_done', None),
+            load_scene=event_dict.get('load_scene', None),
+            clues_necessary=event_dict.get('clues_necessary', []),
+            scene=event_dict.get('scene', None),
+            clue_id=event_dict.get('clue_id', None)
         )
 
     @staticmethod
@@ -40,27 +48,29 @@ class Event:
 
         return events
 
-    def __init__(self, id, dialogue, text_only, character_id=None, hint=None, entry=False, trigger_event_id=None, movements=None, options=[], clue=None, teleport_to=None, trigger_on_done=None):
+    def __init__(self, id, dialogue, text_only, character_id=None, hint=None, trigger=None, movements=None, options=[], clue=None, teleport_to=None, trigger_on_done=None, load_scene=None, clues_necessary=[], scene=None, clue_id=None):
         """ Character should be included if a character is saying the dialogue.
             Hint should be included if interacting with a hint triggers the
-            dialogue. Entry should be True if the dialogue occurs automatically.
-            Event should be given if this event always occurs after the given event.
-            Movements should be a list of movements that the given character should
-            perform after the given dialogue.
+            dialogue. Event should be given if this event always occurs after 
+            the given event. Movements should be a list of movements that the 
+            given character should perform after the given dialogue.
         """
         self.id = id
         self.dialogue = dialogue.replace('\n', ' ')
         self.text_only = text_only
         self.character_id = character_id
         self.hint = hint
-        self.entry = entry
-        self.trigger_event_id = trigger_event_id
+        self.trigger = trigger
         self.movements = movements
-        self.options = options
+        self.options = [Option(option['text'], option['trigger']) for option in options]
         self.selection = 0
         self.clue = clue
         self.teleport_to = teleport_to
         self.trigger_on_done = trigger_on_done
+        self.load_scene = load_scene
+        self.clues_necessary = clues_necessary
+        self.scene = scene
+        self.clue_id = clue_id
 
     def __hash__(self):
         return hash(id)
@@ -99,9 +109,9 @@ class Event:
 
         for i, option in enumerate(self.options):
             if i == self.selection:
-                option = '> ' + option
+                option = '> ' + option.text
             else:
-                option = '  ' + option
+                option = '  ' + option.text
                 
             text = Event.font.render(option, False, [0, 0, 0]) 
 
@@ -110,6 +120,8 @@ class Event:
             else:
                 Event.screen.blit(text, [config.SCREEN_WIDTH - config.TEXT_BOX_WIDTH + 3, offset + config.SCREEN_HEIGHT - config.TEXT_BOX_HEIGHT + 3]) 
 
+            offset += Event.text_height + 3
+
     def selection_up(self):
         self.selection -= 1
         self.selection %= len(self.options)
@@ -117,3 +129,6 @@ class Event:
     def selection_down(self):
         self.selection += 1
         self.selection %= len(self.options)
+
+    def get_selection_trigger_event_id(self):
+        return self.options[self.selection].trigger
