fc2b68050a2ba3d229a0a4bdc9a95f1db5d3230d
Justin Gunther <danwgun@gmail.com>
2019-11-30 18:58:39 -0600

    improved character engine

--- character/character.py
+++ character/character.py
@@ -7,6 +7,14 @@ class Coordinate:
         self.x = x
         self.y = y
 
+    def get_nearby(self):
+        return [
+            Coordinate(self.x - 1, self.y),
+            Coordinate(self.x + 1, self.y),
+            Coordinate(self.x, self.y - 1),
+            Coordinate(self.x, self.y + 1),
+        ]
+
     def __hash__(self):
         return self.x * 10000 + self.y
 
@@ -18,10 +26,13 @@ class Coordinate:
 
 class Character:
     valid_locations = {}
+    clue_locations = []
 
-    def __init__(self, left_images, right_images, up_images, down_images, x, y, speed, boundaries_image_filename, screen, main_character):
-        self.width = config.SADES['width']
-        self.height = config.SADES['height']
+    def __init__(self, id, left_images, right_images, up_images, down_images, x, y, speed, boundaries_image_filename, clues_image_filename, screen, main_character, clue_events=[]):
+        self.id = id
+
+        self.width = config.CHARACTER_WIDTH
+        self.height = config.CHARACTER_HEIGHT
 
         left_images = [pygame.image.load(filename) for filename in left_images]
         right_images = [pygame.image.load(filename) for filename in right_images]
@@ -55,25 +66,91 @@ class Character:
 
         self.main_character = main_character
 
+        self.clue_events = clue_events
+
+        Character.init_clue_locations([clue_event.clue for clue_event in self.clue_events])
+
+    @staticmethod
+    def get_locations_with_color(image_filename, color):
+        pil_image = Image.open(image_filename)
+        width, height = pil_image.size
+        pil_image = pil_image.convert('RGB')
+
+        locations = []
+
+        for i in range(width):
+            for j in range(height):
+                pixel_color = pil_image.getpixel((i, j))
+                if pixel_color[0] == color[0] and pixel_color[1] == color[1] and pixel_color[2] == color[2]:
+                    locations.append(Coordinate(i, j))
+
+        return locations
+
     @staticmethod
     def init_valid_locations(boundaries_image_filename):
         if boundaries_image_filename in Character.valid_locations:
             return
 
-        pil_image = Image.open(boundaries_image_filename)
-        width, height = pil_image.size
-        pil_image = pil_image.convert('RGB')
+        Character.valid_locations[boundaries_image_filename] = set(Character.get_locations_with_color(boundaries_image_filename, [255, 255, 255]))
+   
+    @staticmethod
+    def init_clue_locations(clue_rectangles):
+        for clue_rectangle in clue_rectangles:
+            locations = set()
 
-        valid_locations = set()
+            for i in range(clue_rectangle[0], clue_rectangle[0] + clue_rectangle[2]):
+                for j in range(clue_rectangle[1], clue_rectangle[1] + clue_rectangle[3]):
+                    locations.add(Coordinate(i, j))
 
-        for i in range(width):
-            for j in range(height):
-                pixel_color = pil_image.getpixel((i, j))
-                if sum(pixel_color[0:3]) == 765:
-                    valid_locations.add(Coordinate(i, j))
+            Character.clue_locations.append(locations)
 
-        Character.valid_locations[boundaries_image_filename] = valid_locations
+    def __eq__(self, other):
+        return self.id == other.id
         
+    def __str__(self):
+        return 'ID: {}'.format(self.id)
+
+    def __repr__(self):
+        return str(self)
+
+    def is_clue_location(self, x, y):
+        print x, y
+        print Character.clue_locations
+
+        for i in range(x, x + self.width, self.width / 5):
+            for j in range(y, y + self.height, self.height / 5):
+                for clue_blob in Character.clue_locations:
+                    if Coordinate(i, j) in clue_blob:
+                        return clue_blob
+
+        return None
+
+    def interact(self):
+        y_offset = 0
+        x_offset = 0
+
+        if self.direction == 'up':
+            x_offset = self.width / 2
+            y_offset = -5
+        elif self.direction == 'down':
+            x_offset = self.width / 2
+            y_offset = 5 + self.height
+        elif self.direction == 'right':
+            x_offset = 5 + self.width
+            y_offset = self.height / 2
+        elif self.direction == 'left':
+            x_offset = -5
+            y_offset = self.height / 2
+
+        clue_blob = self.is_clue_location(self.x + x_offset, self.y + y_offset)
+
+        if clue_blob:
+            for clue_event in self.clue_events:
+                if Coordinate(clue_event.clue[0], clue_event.clue[1]) in clue_blob:
+                    return clue_event
+
+        return None
+
     def add_images(self, images_dict, prefix, images):
         for i, image in enumerate(images):
             images_dict[prefix + str(i)] = image
@@ -86,38 +163,46 @@ class Character:
 
         return True
 
-    def draw(self):
+    def draw(self, xy=None):
         self.step %= self.frames
-        if self.main_character:
+        if not xy:
             center = ((config.SCREEN_WIDTH / 2) - (self.width / 2), (config.SCREEN_HEIGHT / 2) - (self.height / 2))
             self.screen.blit(self.images[self.direction + str(self.step)], center)
         else:
-            self.screen.blit(self.images[self.direction + str(self.step)], (self.x, self.y))
+            self.screen.blit(self.images[self.direction + str(self.step)], (xy[0], xy[1]))
 
-    def up(self):
-        if self.is_valid_location(self.x, self.y - self.speed):
+    def up(self, detect=True):
+        if not detect or self.is_valid_location(self.x, self.y - self.speed):
             self.y -= self.speed
 
         self.direction = 'up'
         self.step += 1
     
-    def down(self):
-        if self.is_valid_location(self.x, self.y + self.speed):
+    def down(self, detect=True):
+        if not detect or self.is_valid_location(self.x, self.y + self.speed):
             self.y += self.speed
 
         self.direction = 'down'
         self.step += 1
 
-    def left(self):
-        if self.is_valid_location(self.x - self.speed, self.y):
+    def left(self, detect=True):
+        if not detect or self.is_valid_location(self.x - self.speed, self.y):
             self.x -= self.speed
             
         self.direction = 'left'
         self.step += 1
 
-    def right(self):
-        if self.is_valid_location(self.x + self.speed, self.y):
+    def right(self, detect=True):
+        if not detect or self.is_valid_location(self.x + self.speed, self.y):
             self.x += self.speed
 
         self.direction = 'right'
         self.step += 1
+
+    def get_relative_coordinates(self, x, y):
+        """ Returns coordinates of the scene, assuming this character is the 
+            main character.
+        """
+        relative_x = (config.SCREEN_WIDTH / 2) - (self.width / 2) - self.x + x
+        relative_y = (config.SCREEN_HEIGHT / 2) - (self.height / 2) - self.y + y
+        return relative_x, relative_y
